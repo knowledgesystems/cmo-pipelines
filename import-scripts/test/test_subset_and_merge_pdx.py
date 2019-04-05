306730d27a022c93541986952641c98a2fcfb1b3
Avery Wang <wanga5@mskcc.org>
2019-04-05 11:25:01 -0400

    change to use tempdir

--- import-scripts/test/test_subset_and_merge_pdx.py
+++ import-scripts/test/test_subset_and_merge_pdx.py
@@ -1,10 +1,6 @@
 # run all unit tests with:
 #     import-scripts> python -m unittest discover
 #
-# TODO this only tests process_clinical_file, other methods should be tested too
-# TODO fix the removing data bug
-# TODO add some unit tests which explore the different modes related to --force option and overwriting
-#
 # Author: Avery Wang
 
 import unittest
@@ -20,26 +16,50 @@ class TestSubsetAndMergePDXStudies(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         resource_dir = "test/resources/subset_and_merge_pdx/"
-        data_repos = "data_repos/"
-        expected_files = "expected_output/"
-        cls.root_directory = os.path.join(resource_dir, data_repos, "crdb_pdx_repos/")
-        cls.dmp_directory = os.path.join(resource_dir, data_repos, "dmp_source_repo/")
-        cls.cmo_directory = os.path.join(resource_dir, data_repos, "cmo_source_repo/")
-        cls.datahub_directory = os.path.join(resource_dir, data_repos, "datahub_source_repo/")
+        data_repos = os.path.join(resource_dir, "data_repos/")
+        cls.expected_files = os.path.join(resource_dir, "expected_output")
+        
+        # move all data into a temporary directory for manipulation
+        cls.temp_dir = os.path.join(resource_dir, "tmp")
+        shutil.copytree(data_repos, cls.temp_dir)
+        
+        cls.lib = "./"
+        cls.root_directory = os.path.join(cls.temp_dir, "crdb_pdx_repos/")
+        cls.dmp_directory = os.path.join(cls.temp_dir, "dmp_source_repo/")
+        cls.cmo_directory = os.path.join(cls.temp_dir, "cmo_source_repo/")
+        cls.datahub_directory = os.path.join(cls.temp_dir, "datahub_source_repo/")
+        
         cls.crdb_fetch_directory = os.path.join(cls.root_directory, "crdb_pdx_raw_data/")
         cls.destination_to_source_mapping_file = os.path.join(cls.crdb_fetch_directory, "source_to_destination_mappings.txt")
         cls.clinical_annotations_mapping_file = os.path.join(cls.crdb_fetch_directory, "clinical_annotations_mappings.txt")
-
+    
+    @classmethod
+    def tearDownClass(cls):
+        # clean up copied tempdir/data
+        shutil.rmtree(cls.temp_dir)
+    
     def test_load_destination_source_patient_mappings(self):
-        # parse the two mapping files provided (which patients and which clinical attributes)
         destination_source_patient_mapping_records = parse_file(self.destination_to_source_mapping_file)
         destination_source_clinical_annotation_mapping_records = parse_file(self.clinical_annotations_mapping_file)
-        # create a dictionary mapping (destination - source) to a SourceMappings object
         destination_to_source_mapping = create_destination_to_source_mapping(destination_source_patient_mapping_records, destination_source_clinical_annotation_mapping_records, self.root_directory)
+        
         self.check_destination_source_mappings(destination_to_source_mapping) 
         self.check_destination_source_clinical_annotations_mappings(destination_to_source_mapping)
         self.check_destination_source_patient_mappings(destination_to_source_mapping)
+        
+        source_id_to_path_mapping = create_source_id_to_path_mapping(destination_to_source_mapping, [self.datahub_directory, self.cmo_directory, self.dmp_directory], self.crdb_fetch_directory)
+        subset_source_directories(destination_to_source_mapping, source_id_to_path_mapping, self.root_directory, self.lib)
 
+        self.check_source_subsets(destination_to_source_mapping)
+    
+    def check_source_subsets(self, destination_to_source_mapping):
+        for destination, source_to_source_mapping in destination_to_source_mapping.items():
+            for source, source_mapping in source_to_source_mapping.items():
+                expected_directory = os.path.join(self.expected_files, "subset_source_step", destination, source)
+                subsetted_directory = os.path.join(self.root_directory, destination, source)
+                print "Will compare: " + expected_directory + " to " + subsetted_directory
+
+ 
     def check_destination_source_mappings(self, destination_to_source_mapping):
         expected_destination_ids = ["test_destination_study_1", "test_destination_study_2"]
         expected_source_ids = ["test_source_study_1", "cmo_test_source_study_1", "test_msk_solid_heme", "crdb_pdx_raw_data"] 
@@ -87,6 +107,6 @@ class TestSubsetAndMergePDXStudies(unittest.TestCase):
         self.assertEquals(os.path.join(self.cmo_directory, "cmo/test/source/study_1"), resolve_source_study_path("cmo_test_source_study_1", [self.datahub_directory, self.cmo_directory, self.dmp_directory]))
         self.assertEquals(os.path.join(self.dmp_directory, "test_msk_solid_heme"), resolve_source_study_path("test_msk_solid_heme", [self.datahub_directory, self.cmo_directory, self.dmp_directory]))
         self.assertFalse(resolve_source_study_path("fake_study_path", [self.datahub_directory, self.cmo_directory, self.dmp_directory]))
- 
+
 if __name__ == '__main__':
     unittest.main()
