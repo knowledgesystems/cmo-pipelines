5b04e1761bc2f7363ac442ade94043fd559fa79c
Avery Wang <wanga5@mskcc.org>
2019-04-08 15:40:13 -0400

    break tests into seperate steps - up to merge step

--- import-scripts/test/test_subset_and_merge_pdx.py
+++ import-scripts/test/test_subset_and_merge_pdx.py
@@ -17,14 +17,14 @@ class TestSubsetAndMergePDXStudies(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         resource_dir = "test/resources/subset_and_merge_pdx/"
-        data_repos = os.path.join(resource_dir, "data_repos/")
+        cls.data_repos = os.path.join(resource_dir, "data_repos/")
         cls.expected_files = os.path.join(resource_dir, "expected_outputs")
         
         # move all data into a temporary directory for manipulation
         cls.temp_dir = os.path.join(resource_dir, "tmp")
         if os.path.isdir(cls.temp_dir):
             shutil.rmtree(cls.temp_dir)
-        shutil.copytree(data_repos, cls.temp_dir)
+        shutil.copytree(cls.data_repos, cls.temp_dir)
         
         cls.lib = "./"
         cls.root_directory = os.path.join(cls.temp_dir, "crdb_pdx_repos/")
@@ -35,6 +35,10 @@ class TestSubsetAndMergePDXStudies(unittest.TestCase):
         cls.crdb_fetch_directory = os.path.join(cls.root_directory, "crdb_pdx_raw_data/")
         cls.destination_to_source_mapping_file = os.path.join(cls.crdb_fetch_directory, "source_to_destination_mappings.txt")
         cls.clinical_annotations_mapping_file = os.path.join(cls.crdb_fetch_directory, "clinical_annotations_mappings.txt")
+        
+        cls.destination_source_patient_mapping_records = parse_file(cls.destination_to_source_mapping_file)
+        cls.destination_source_clinical_annotation_mapping_records = parse_file(cls.clinical_annotations_mapping_file)
+        cls.destination_to_source_mapping = create_destination_to_source_mapping(cls.destination_source_patient_mapping_records, cls.destination_source_clinical_annotation_mapping_records, cls.root_directory)
     
     @classmethod
     def tearDownClass(cls):
@@ -48,29 +52,11 @@ class TestSubsetAndMergePDXStudies(unittest.TestCase):
         self.assertEquals(os.path.join(self.dmp_directory, "test_msk_solid_heme"), resolve_source_study_path("test_msk_solid_heme", [self.datahub_directory, self.cmo_directory, self.dmp_directory]))
         self.assertFalse(resolve_source_study_path("fake_study_path", [self.datahub_directory, self.cmo_directory, self.dmp_directory])) 
     
-    def test_load_destination_source_patient_mappings(self):
-        destination_source_patient_mapping_records = parse_file(self.destination_to_source_mapping_file)
-        destination_source_clinical_annotation_mapping_records = parse_file(self.clinical_annotations_mapping_file)
-        destination_to_source_mapping = create_destination_to_source_mapping(destination_source_patient_mapping_records, destination_source_clinical_annotation_mapping_records, self.root_directory)
-        
-        self.check_destination_source_mappings(destination_to_source_mapping) 
-        self.check_destination_source_clinical_annotations_mappings(destination_to_source_mapping)
-        self.check_destination_source_patient_mappings(destination_to_source_mapping)
-        
-        source_id_to_path_mapping = create_source_id_to_path_mapping(destination_to_source_mapping, [self.datahub_directory, self.cmo_directory, self.dmp_directory], self.crdb_fetch_directory)
-        subset_source_directories(destination_to_source_mapping, source_id_to_path_mapping, self.root_directory, self.lib)
-        self.check_subset_source_step(destination_to_source_mapping)
-
-        remove_unwanted_clinical_annotations_in_subsetted_source_studies(destination_to_source_mapping, self.root_directory)
-        self.compare_clinical_files_for_step(destination_to_source_mapping, "filter_clinical_annotations_step")
-
-        convert_cmo_to_dmp_pids_in_subsetted_source_studies(destination_to_source_mapping, self.root_directory)
-        self.compare_clinical_files_for_step(destination_to_source_mapping, "rename_patients_step")
-
-        #merge_source_directories(destination_to_source_mapping, self.root_directory, self.lib)
-        #remove_source_subdirectories(destination_to_source_mapping, self.root_directory)
-        #self.check_merge_source_directories_step(destination_to_source_mapping, self.root_directory)
-   
+    def test_load_destination_source_patient_mappings(self):    
+        self.check_destination_source_mappings(self.destination_to_source_mapping) 
+        self.check_destination_source_clinical_annotations_mappings(self.destination_to_source_mapping)
+        self.check_destination_source_patient_mappings(self.destination_to_source_mapping)
+ 
     # Step 1a: check source-destination mappings
     def check_destination_source_mappings(self, destination_to_source_mapping):
         expected_destination_ids = ["test_destination_study_1", "test_destination_study_2"]
@@ -116,13 +102,49 @@ class TestSubsetAndMergePDXStudies(unittest.TestCase):
                     self.assertFalse(source_mapping.clinical_annotations)
 
     # test that cmo studies will be found by substituting underscores
+    def test_subset_source_step(self):
+        shutil.rmtree(self.temp_dir)
+        shutil.copytree(self.data_repos, self.temp_dir)
+        source_id_to_path_mapping = create_source_id_to_path_mapping(self.destination_to_source_mapping, [self.datahub_directory, self.cmo_directory, self.dmp_directory], self.crdb_fetch_directory)
+        subset_source_directories(self.destination_to_source_mapping, source_id_to_path_mapping, self.root_directory, self.lib)
+        self.check_subset_source_step(self.destination_to_source_mapping)
+
+    def test_filter_clinical_annotations_step(self):
+        shutil.rmtree(self.root_directory)
+        for destination, source_to_source_mappings in self.destination_to_source_mapping.items():
+            expected_directory = os.path.join(self.expected_files, "subset_source_step", destination)
+            actual_directory = os.path.join(self.root_directory, destination)
+            shutil.copytree(expected_directory, actual_directory)
+        remove_unwanted_clinical_annotations_in_subsetted_source_studies(self.destination_to_source_mapping, self.root_directory)
+        self.compare_clinical_files_for_step(self.destination_to_source_mapping, "filter_clinical_annotations_step")
+
+    def test_rename_patients_step(self):
+        shutil.rmtree(self.root_directory)
+        for destination, source_to_source_mappings in self.destination_to_source_mapping.items():
+            expected_directory = os.path.join(self.expected_files, "filter_clinical_annotations_step", destination)
+            actual_directory = os.path.join(self.root_directory, destination)
+            shutil.copytree(expected_directory, actual_directory)
+        convert_cmo_to_dmp_pids_in_subsetted_source_studies(self.destination_to_source_mapping, self.root_directory)
+        self.compare_clinical_files_for_step(self.destination_to_source_mapping, "rename_patients_step")
+
+    def test_merge_source_directories_step(self):
+        shutil.rmtree(self.root_directory)
+        for destination, source_to_source_mappings in self.destination_to_source_mapping.items():
+            expected_directory = os.path.join(self.expected_files, "rename_patients_step", destination)
+            actual_directory = os.path.join(self.root_directory, destination)
+            shutil.copytree(expected_directory, actual_directory)
+        merge_source_directories(self.destination_to_source_mapping, self.root_directory, self.lib)
+        remove_source_subdirectories(self.destination_to_source_mapping, self.root_directory)
+        self.check_merge_source_directories_step(self.destination_to_source_mapping)
+   
+        #merge_clinical_files(destination_to_source_mapping, root_directory, lib)
+        #self.check_merge_clinical_files_step(destination_to_source_mapping, "merge_clinical_files_step")
+
     def check_subset_source_step(self, destination_to_source_mapping):
         for destination, source_to_source_mapping in destination_to_source_mapping.items():
             for source, source_mapping in source_to_source_mapping.items():
                 expected_directory = os.path.join(self.expected_files, "subset_source_step", destination, source)
                 actual_directory = os.path.join(self.root_directory, destination, source)
-                if not "test_destination_study_2" in actual_directory:
-                    continue
                 for datafile in os.listdir(expected_directory):
                     self.assertTrue(self.sort_and_compare_files(os.path.join(actual_directory, datafile), os.path.join(expected_directory, datafile)))
     
@@ -131,8 +153,6 @@ class TestSubsetAndMergePDXStudies(unittest.TestCase):
             for source, source_mapping in source_to_source_mapping.items():
                 expected_directory = os.path.join(self.expected_files, step_name, destination, source)
                 actual_directory = os.path.join(self.root_directory, destination, source)
-                if not "test_destination_study_2" in actual_directory:
-                    continue
                 for clinical_file in [filename for filename in os.listdir(expected_directory) if "data_clinical" in filename]:
                     self.assertTrue(self.sort_and_compare_files(os.path.join(actual_directory, clinical_file), os.path.join(expected_directory, clinical_file)))
 
@@ -140,9 +160,7 @@ class TestSubsetAndMergePDXStudies(unittest.TestCase):
         for destination in destination_to_source_mapping:
             expected_directory = os.path.join(self.expected_files, "merge_source_directories_step", destination)
             actual_directory = os.path.join(self.root_directory, destination)
-            if not "test_destination_study_2" in actual_directory:
-                 continue
-            for clinical_file in [filename for filename in os.listdir(expected_directory) if "meta" not in filename]:
+            for clinical_file in [filename for filename in os.listdir(expected_directory) if "meta" not in filename and "temp" not in filename]:
                 self.assertTrue(self.sort_and_compare_files(os.path.join(actual_directory, clinical_file), os.path.join(expected_directory, clinical_file)))
 
     def sort_and_compare_files(self, actual_file, expected_file):
