f07f3ef0aba4ab3f5f2ac9f3a9cb34347614784e
Avery Wang <averyjwang@gmail.com>
2021-07-29 10:54:51 -0700

    split scripts into eks vs pipelines deployed

--- import-scripts/monitor-crontab-version.sh
+++ import-scripts/monitor-crontab-version.sh
@@ -14,20 +14,30 @@ if [ -z $PORTAL_HOME ] | [ -z $PIPELINES_CONFIG_HOME ] ; then
     exit 1
 fi
 
-GITHUB_CRONTAB_URL="https://api.github.com/repos/knowledgesystems/cmo-pipelines/contents/import-scripts/mycrontab"
+GITHUB_PIPELINES_CRONTAB_URL="https://api.github.com/repos/knowledgesystems/cmo-pipelines/contents/import-scripts/pipelines/mycrontab"
+GITHUB_PIPELINES_EKS_CRONTAB_URL="https://api.github.com/repos/knowledgesystems/cmo-pipelines/contents/import-scripts/pipelines_eks/mycrontab"
 GITHUB_AUTHORIZATION=$(cat $PIPELINES_CONFIG_HOME/git/git-credentials | sed 's/https:\/\///; s/@github\.com//')
 CURRENT_CRONTAB_FILE="$PORTAL_HOME/tmp/current_crontab"
 GITHUB_CRONTAB_FILE="$PORTAL_HOME/tmp/git_crontab"
 REQUIRED_USERNAME="cbioportal_importer"
-REQUIRED_HOSTNAME="pipelines.cbioportal.mskcc.org"
+PIPELINES_HOSTNAME="pipelines.cbioportal.mskcc.org"
+PIPELINES_EKS_HOSTNAME="ip-10-1-21-42.mskcc.root.mskcc.org"
 
 if [ $USER != $REQUIRED_USERNAME ] ; then
     echo "monitor-crontab-version.sh must be run as $REQUIRED_USERNAME"
     exit 1
 fi
 
-if [ $HOSTNAME != $REQUIRED_HOSTNAME ] ; then
-    echo "monitor-crontab-version.sh  must be run on $HOSTNAME"
+if [ $HOSTNAME == $PIPELINES_HOSTNAME" ] ; then
+    GITHUB_CRONTAB_URL=$GITHUB_PIPELINES_CRONTAB_URL
+fi
+
+if [ $HOSTNAME == $PIPELINES_EKS_HOSTNAME" ] ; then
+    GITHUB_CRONTAB_URL=$GITHUB_PIPELINES_EKS_CRONTAB_URL
+fi
+
+if [ -z $GITHUB_CRONTAB_URL ] ; then
+    echo "monitor-crontab-version.sh  must be run on either $PIPELINES_HOSTNAME or $PIPELINES_EKS_HOSTNAME"
     exit 1
 fi
 
