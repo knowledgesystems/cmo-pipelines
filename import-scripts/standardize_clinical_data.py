77e51b36109cbec162b042c3d83234f08bb7e910
averyniceday <wanga5@mskcc.org>
2023-01-18 08:54:27 -0800

    Update standardize_clinical_data.py

--- import-scripts/standardize_clinical_data.py
+++ import-scripts/standardize_clinical_data.py
@@ -9,23 +9,22 @@ OUTPUT_FILE = sys.stdout
 
 def main():
     # get command line stuff
-    parser = argparse.ArgumentParser(prog="filter_clinical_data.py")
-    parser.add_argument('-c', '--clinical-file', dest='clinical_filename', action='store', required=True, help='path to clinical file')
+    parser = argparse.ArgumentParser(prog="standardize_datafile.py")
+    parser.add_argument('-f', '--filename', dest='filename', action='store', required=True, help='path to clinical file')
 
     args = parser.parse_args()
-    clinical_filename = args.clinical_filename
+    filename = args.filename
 
     # check arguments
-    if not os.path.exists(clinical_filename):
-        print >> ERROR_FILE, "No such file: " + clinical_filename
+    if not os.path.exists(filename):
+        print >> ERROR_FILE, "No such file: " + filename
         parser.print_help()
 
     # remove columns from the clinical file
     try:
-        clinicalfile_utils.write_standardized_columns(clinical_filename, OUTPUT_FILE)
+        clinicalfile_utils.write_standardized_columns(filename, OUTPUT_FILE)
     except ValueError as error:
-        print >> ERROR_FILE, "One or more of the input columns was not found in the header:", error
-        print >> ERROR_FILE, "No columns have been removed from the clinical file"
+        print >> ERROR_FILE, "Unable to write standardized file. Exiting..."
         sys.exit(2) 
 
 if __name__ == '__main__':
