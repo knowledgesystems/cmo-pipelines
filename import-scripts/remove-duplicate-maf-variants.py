a9544db50eeed0a769af569256341f79942705e0
Calla Chennault <51501715+callachennault@users.noreply.github.com>
2024-01-29 16:21:39 -0500

    Add comments

--- import-scripts/remove-duplicate-maf-variants.py
+++ import-scripts/remove-duplicate-maf-variants.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
-import sys
-import os
-import optparse
 
-# Script to remove duplicate maf records based on the 8 key columns.
-# Calculates VAF for each record and picks the record with high VAF
-# Formula for VAF = t_alt_count / (t_ref_count + t_alt_count) 
+""" remove-duplicate-maf-variants.py
+Script to remove duplicate maf records based on the 8 key columns.
+Calculates VAF for each record and picks the record with high VAF
+Formula for VAF = t_alt_count / (t_ref_count + t_alt_count)
+"""
+
+import sys
+import optparse
 
 ERROR_FILE = sys.stderr
 OUTPUT_FILE = sys.stdout
@@ -14,7 +16,7 @@ KEY_COLUMNS_INDEX = []
 KEY_COLUMNS = ['Entrez_Gene_Id','Chromosome','Start_Position','End_Position','Variant_Classification','Tumor_Seq_Allele2','Tumor_Sample_Barcode','HGVSp_Short']
 MAF_DATA = {}
 
-def remove_duplicate_variants(maf_filename, out_filename, comments, header, t_refc_index, t_altc_index):
+def remove_duplicate_variants(out_filename, comments, header, t_refc_index, t_altc_index):
 	outfile = []
 	outfile.append(comments)
 	outfile.append(header)
@@ -81,7 +83,7 @@ def main():
 				else:
 					MAF_DATA[reference_key].append(line)
 	
-	remove_duplicate_variants(maf_filename, out_filename, comments, header, t_refc_index, t_altc_index)
+	remove_duplicate_variants(out_filename, comments, header, t_refc_index, t_altc_index)
 
 if __name__ == '__main__':
 	main()
--- import-scripts/transpose_cna_py3.py
+++ import-scripts/transpose_cna_py3.py
@@ -1,12 +1,31 @@
+#!/usr/bin/env python3
+
+""" transpose_cna_py3.py
+This script transposes the data_CNA.txt file so that sample IDs are in the first column rather than across the header.
+This format was requested to improve ease of anonymizing sample IDs for the Sophia Genetics data.
+
+Usage:
+    python3 transpose_cna_py3.py $INPUT_DATA_CNA_FILE_PATH
+Example:
+    python3 transpose_cna_py3.py path/to/data_cna.txt \
+"""
+
 import argparse
 import os
 import pandas as pd
 
 
 def transpose_and_write_cna_file(data_cna_file):
+    # Reads CNA file, skipping comments
     df = pd.read_csv(data_cna_file, sep='\t', comment='#')
+
+    # Renames the Hugo_Symbol column so that the column header is correct when transposed
     df = df.rename(columns={'Hugo_Symbol': 'SAMPLE_ID'})
+
+    # Transposes the data frame
     df = df.T
+
+    # Writes the transposed data frame to the original file path
     df.to_csv(data_cna_file, sep='\t', header=None)
 
 
