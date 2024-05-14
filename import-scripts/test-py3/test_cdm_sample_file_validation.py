cb49003e5f357d93c63eb14f28da1eb8358530fa
Calla Chennault <51501715+callachennault@users.noreply.github.com>
2024-05-14 15:57:21 -0400

    Rename tests and update tests

--- import-scripts/test-py3/test_cdm_sample_file_validation.py
+++ import-scripts/test-py3/test_cdm_sample_file_validation.py
@@ -10,27 +10,27 @@ import os
 
 from validation_utils_py3 import CDMSampleFileValidator
 
-class TestSampleFileValidation(unittest.TestCase):
+class TestCDMSampleFileValidation(unittest.TestCase):
 
     # Show the diff even if it is very big
     maxDiff = None
 
     @classmethod
     def setUpClass(cls):
-        cls.base_dir = 'test-py3/resources/validation_utils/sample_file'
+        cls.base_dir = "test-py3/resources/validation_utils/cdm_sample_file"
 
     def test_sample_file_no_changes(self):
-        sub_dir = 'no_changes'
+        sub_dir = "no_changes"
         self.compare_expected_output_to_actual(sub_dir)
 
-    #def test_sample_file_mismatched_ids(self):
-    #    sub_dir = 'mismatched_ids'
-    #    self.compare_expected_output_to_actual(sub_dir)
+    def test_sample_file_mismatched_ids(self):
+        sub_dir = "mismatched_ids"
+        self.compare_expected_output_to_actual(sub_dir)
   
     def compare_expected_output_to_actual(self, sub_dir):
-        input_sample_file = os.path.join(TestSampleFileValidation.base_dir, sub_dir, "data_clinical_sample.txt")
-        output_file = os.path.join(TestSampleFileValidation.base_dir, sub_dir, 'data_clinical_sample_output.txt')
-        expected_file = os.path.join(TestSampleFileValidation.base_dir, sub_dir, 'data_clinical_sample_expected.txt')
+        input_sample_file = os.path.join(TestCDMSampleFileValidation.base_dir, sub_dir, "data_clinical_sample.txt")
+        output_file = os.path.join(TestCDMSampleFileValidation.base_dir, sub_dir, "data_clinical_sample_output.txt")
+        expected_file = os.path.join(TestCDMSampleFileValidation.base_dir, sub_dir, "data_clinical_sample_expected.txt")
 
         sample_file_validator = CDMSampleFileValidator(input_sample_file, output_file_path=output_file)
 
@@ -42,8 +42,8 @@ class TestSampleFileValidation(unittest.TestCase):
             raise
 
         # Read output file and compare it to expected output
-        with open(expected_file, 'r') as expected_out:
-            with open(output_file, 'r') as actual_out:
+        with open(expected_file, "r") as expected_out:
+            with open(output_file, "r") as actual_out:
                 self.assertEqual(expected_out.read(), actual_out.read())
 
         # Clean up output file
