ac76498e203e2cbc966462d300b60034cdd92d1b
Calla Chennault <51501715+callachennault@users.noreply.github.com>
2024-05-14 16:36:10 -0400

    Update comments

--- import-scripts/validation_utils_py3.py
+++ import-scripts/validation_utils_py3.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
 """ validation_utils_py3.py
-This can be used to validate the format of a .
-Its primary use is to:
-- merge DDP files from MSK-IMPACT, HEMEPACT, ACCESS to generate merged DDP files
-for the msk_solid_heme cohort. This merged DDP file is only used for GENIE cohort creation.
+This script can be used to validate the format of an arbitrary study file.
+As of 5/14/24 only CDM clinical sample file validation has been implemented but
+this should be extended for future use-cases.
+
 Usage:
-    python3 combine_files_py3.py --input-files $FILE1 $FILE2 <...> --output-file $OUTPUT_FILE
+    python3 validation_utils_py3.py --validation-type $VALIDATION_TYPE --file-path $FILE_PATH
 Example:
-    python3 combine_files_py3.py --input-files mskimpact/ddp/ddp_naaccr.txt 
+    python3 validation_utils_py3.py --validation-type cdm --file-path data_clinical_sample.txt 
 """
 
 
@@ -33,6 +33,14 @@ class FileValidator(ABC):
     # Define other base-level file checks here
     
     def load_file(self, sep="\t", float_precision="round_trip", na_filter=False, low_memory=False, **opts):
+        """ Loads the given file into a pandas dataframe. 
+            Accepts an arbitrary number of arguments to pass to the pandas read_table function,
+            with defaults for the following arguments:
+                sep="\t" : Tab delimiter for the input file
+                float_precision="round_trip" : Prevent floating point rounding
+                na_filter=False : Prevent interpreting of NA values
+                low_memory=False : Prevent mixed type inference
+        """
         start_read = 0
         with open(self.file_path, "r") as f:
             # Assumes that commented lines are at the start of the file
@@ -55,18 +63,23 @@ class FileValidator(ABC):
 
         # TODO define a return value?
 
-    # Sub-classes must define this method
     @abstractmethod
     def run_validate_checks(self):
+        """ Abstract method that must be defined by sub-classes.
+            Should contain all validation checks required for file type.
+        """
         ...
 
-    # Top-level method to validate file
-    # Sub-classes need to define run_validate_checks()
     def validate(self):
+        """ Top-level method to validate the given file by loading it into a dataframe,
+            running all validation checks, writing the validated data to a file, and clearing the dataframe.
+            Sub-classes need to define run_validate_checks().
+        """
+
         # Validate that the file exists before running any checks
         assert self.validate_file_exists(), f"File {self.file_path} does not exist"
 
-        # TODO assert statements for all steps ?
+        # TODO assert statements for all steps
 
         # Load file contents into dataframe
         self.load_file()
@@ -80,21 +93,28 @@ class FileValidator(ABC):
         # Clear df from memory
         self.clear_df()
 
-    def write_to_file(self, sep="\t", mode="a", quoting=csv.QUOTE_NONE, **opts):
+    def write_to_file(self, sep="\t", mode="a", quoting=csv.QUOTE_NONE, index=False, **opts):
+        """ Writes the validated file contents out to a file.
+            Accepts an arbitrary number of arguments to pass to the pandas to_csv function,
+            with defaults for the following arguments:
+                sep="\t" : Tab delimiter for the output file
+                mode="a" : Append to the end of the output file if it exists (so that we do not overrwite the file header)
+                quoting=csv.QUOTE_NONE : Prevent pandas from adding extra quotes into string fields
+                index=False : By default, don't write the index column to the output
+        """
+
         # Write header to file
         with open(self.output_file_path, "w") as f:
             for line in self.header:
                 f.write(f"{line}\n")
 
         # Write data to file
-        opts["index"] = opts.get(
-            "index", False
-        )  # by default, don't write the index column to the output
         self.df.to_csv(
             self.output_file_path,
             sep=sep,
             mode=mode,
             quoting=quoting,
+            index=index,
             **opts,
         )
 
@@ -103,29 +123,36 @@ class FileValidator(ABC):
 
 
 class CDMSampleFileValidator(FileValidator):
+    """Class to validate the clinical sample file provided by CDM."""
+
     def __init__(self, file_path, output_file_path=""):
         super().__init__(file_path, output_file_path)
     
     def validate_sids_match_pids(self):
+        """ Extracts the patient ID from the SAMPLE_ID column and verifies that it matches the PATIENT_ID column for each
+            row in the dataframe. If the two do not match, the row is removed.
+        """
         non_matching = self.df.query("PATIENT_ID != SAMPLE_ID.str.extract('(P-[0-9]*)-*')[0]")
         self.df = self.df.drop(non_matching.index)
 
         if len(non_matching.index) > 0:
             logging.warning(f"The following {len(non_matching.index)} records were dropped due to mismatched patient and sample IDs:\n{non_matching}")
 
-    # TODO Add options to run specific checks
     def run_validate_checks(self):
+        # TODO Add options to run specific checks
         self.validate_sids_match_pids()
 
 
 class CDMFileValidator:
+    """Class to validate all CDM data. Currently, this class only validates the clinical sample file."""
+
     def __init__(self, sample_file_path):
         self.sample_file_validator = CDMSampleFileValidator(sample_file_path)
 
     def validate_sample_file(self):
         self.sample_file_validator.validate()
 
-    # Define other file type validation functions here
+    # TODO Define other file type validation functions here
 
     def validate(self):
         self.validate_sample_file()
@@ -137,11 +164,12 @@ if __name__ == "__main__":
         "-v",
         "--validation-type",
         dest="validation_type",
-        choices=["cdm"], # Add here as more validators implemented
+        choices=["cdm"], # Add here as more validators are implemented
         action="store",
         required=True,
         help="Type of validation to run. Accepted values: ['cdm', ]",
     )
+    # May need this argument as more file types are implemented per validator
     """
     parser.add_argument(
         "-t",
