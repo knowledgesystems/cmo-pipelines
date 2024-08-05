3b92edbd3296a1ed7dbe6879d068adebb7b7b123
Calla Chennault <51501715+callachennault@users.noreply.github.com>
2024-08-05 16:27:15 -0400

    Update comment

--- import-scripts/update-cdm-deliverable.sh
+++ import-scripts/update-cdm-deliverable.sh
@@ -53,7 +53,7 @@ fi
 $PYTHON3_BINARY $PORTAL_HOME/scripts/combine_files_py3.py -i "$MSK_ACCESS_SEQ_DATE" "$MSK_HEMEPACT_SEQ_DATE" "$MSK_IMPACT_SEQ_DATE" -o "$TMP_MERGED_SEQ_DATE" -m outer
 
 # Combines filtered clinical sample file with seq data file and outputs to tmp file for upload
-# Uses left join -- SAMPLE_ID in the clinical_sample_file (first arg) will be valid keys
+# Uses left join -- SAMPLE_ID and PATIENT_ID in the clinical_sample_file (first arg) will be valid keys
 $PYTHON3_BINARY $PORTAL_HOME/scripts/combine_files_py3.py -i "$TMP_SAMPLE_FILE" "$TMP_MERGED_SEQ_DATE" -o "$CDM_DELIVERABLE" -c SAMPLE_ID PATIENT_ID -m left
 if [ $? -ne 0 ] ; then
   echo "`date`: Failed to combine files, exiting..."
