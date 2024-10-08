1b5bc4349a2fb0bdefc3889fd99b8b08683e33f4
Calla Chennault <51501715+callachennault@users.noreply.github.com>
2024-10-08 14:24:55 -0400

    Try without re

--- import-scripts/merge.py
+++ import-scripts/merge.py
@@ -70,6 +70,9 @@ RPPA_META_PATTERN = 'meta_rppa.txt'
 TIMELINE_FILE_PATTERN = re.compile('^data_timeline.*\.txt$')
 TIMELINE_META_PATTERN = re.compile('^meta_timeline.*\.txt$')
 
+TIMELINE_FILE_PATTERN = 'data_timeline'
+TIMELINE_META_PATTERN = 'meta_timeline'
+
 CLINICAL_PATIENT_FILE_PATTERN = 'data_clinical_patient.txt'
 CLINICAL_PATIENT_META_PATTERN = 'meta_clinical_patient.txt'
 
@@ -774,7 +777,9 @@ def organize_files(studies, file_types, merge_clinical):
                     file_types[GENE_MATRIX_META_PATTERN].append(study_file)
                 elif SV_META_PATTERN in study_file:
                     file_types[SV_META_PATTERN].append(study_file)
-                elif re.search(TIMELINE_META_PATTERN, study_file):
+                #elif re.search(TIMELINE_META_PATTERN, study_file):
+                elif TIMELINE_META_PATTERN in study_file:
+                    print >> OUTPUT_FILE, 'TIMELINE META PATTERN FOUND: ' + study_file
                     file_types[TIMELINE_META_PATTERN].append(study_file)
                 # FILE PATTERN MATCHING
                 elif MUTATION_FILE_PATTERN in study_file:
@@ -809,8 +814,10 @@ def organize_files(studies, file_types, merge_clinical):
                     file_types[GENE_MATRIX_FILE_PATTERN].append(study_file)
                 elif SV_FILE_PATTERN in study_file:
                     file_types[SV_FILE_PATTERN].append(study_file)
-                elif re.search(TIMELINE_FILE_PATTERN, study_file):
+                #elif re.search(TIMELINE_FILE_PATTERN, study_file):
+                elif TIMELINE_FILE_PATTERN in study_file:
                     file_types[TIMELINE_FILE_PATTERN].append(study_file)
+                    print >> OUTPUT_FILE, 'TIMELINE FILE PATTERN FOUND: ' + study_file
                 # CLINICAL FILE PATTERN MATCHING
                 elif CLINICAL_META_PATTERN in study_file:
                     file_types[CLINICAL_META_PATTERN].append(study_file)
