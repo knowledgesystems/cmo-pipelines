8023baeb45469b2e2df96a91dea2f452a593a08c
Calla Chennault <51501715+callachennault@users.noreply.github.com>
2024-10-31 12:35:09 -0400

    Check-in Avery's scripts

---
+++ import-scripts/clone_db_wrapper.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Uses clone_mysql_database.sh script to clone from one db to another
+# Figures out which database is "production" versus "not production" and passes it to the clone_mysql_database script
+# Also determines which one is "blue"/"green" for the deployment
+
+. /data/portal-cron/scripts/automation-environment.sh
+
+tmp=$PORTAL_HOME/tmp/import-cron-genie
+if ! [ -d "$tmp" ] ; then
+    if ! mkdir -p "$tmp" ; then
+        echo "Error : could not create tmp directory '$tmp'" >&2
+        exit 1
+    fi
+fi
+if [[ -d "$tmp" && "$tmp" != "/" ]]; then
+    rm -rf "$tmp"/*
+fi
+
+# This file will be used throughout entire import job to determine directionality
+SET_UPDATE_PROCESS_OUTPUT_FILEPATH="$tmp/update_process_output.txt"
+MANAGE_DATABASE_TOOL_PROPERTIES_FILEPATH=$PORTAL_HOME/scripts/airflowdb.properties
+DROP_TABLES_FROM_MYSQL_DATABASE_SCRIPT_FILEPATH=$PORTAL_HOME/scripts/drop_tables_in_mysql_database.sh
+CLONE_MYSQL_DATABASE_SCRIPT_FILEPATH=$PORTAL_HOME/scripts/clone_mysql_database.sh
+
+if ! $SET_UPDATE_PROCESS_SCRIPT_FILEPATH $MANAGE_DATABASE_TOOL_PROPERTIES_FILEPATH running > "$SET_UPDATE_PROCESS_OUTPUT_FILEPATH" ; then
+    echo "Error during execution of $SET_UPDATE_PROCESS_SCRIPT_FILEPATH : could not set running state" >&2
+    exit 1
+    #TODO : recover from this error
+fi
+
+current_production_database_color=$(sh $PORTAL_HOME/scripts/get_database_currently_in_production.sh $MANAGE_DATABASE_TOOL_PROPERTIES_FILEPATH)
+source_database_color="unset"
+destination_database_color="unset"
+if [ ${current_production_database_color:0:4} == "blue" ] ; then
+    source_database_color="blue"
+    destination_database_color="green"
+fi
+if [ ${current_production_database_color:0:5} == "green" ] ; then
+    source_database_color="green"
+    destination_database_color="blue"
+fi
+if [ "$destination_database_color" == "unset" ] ; then
+    echo "Error during determination of the destination database color" >&2
+    exit 1
+fi
+
+MYSQL_DATABASE_CLONE_FAIL=0
+if ! $DROP_TABLES_FROM_MYSQL_DATABASE_SCRIPT_FILEPATH $MANAGE_DATABASE_TOOL_PROPERTIES_FILEPATH $destination_database_color ; then
+    message="Error during dropping of tables from mysql database $destination_database_color"
+    echo $message >&2 # TODO in other parts of this script we are writing these error messages to stdout
+    exit 1
+else
+    if ! $CLONE_MYSQL_DATABASE_SCRIPT_FILEPATH $MANAGE_DATABASE_TOOL_PROPERTIES_FILEPATH $source_database_color $destination_database_color ; then
+        message="Error during cloning the mysql database (from $source_database_color to $destination_database_color)"
+        echo $message >&2 # TODO in other parts of this script we are writing these error messages to stdout
+        exit 1
+    fi
+fi
