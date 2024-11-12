c781efee127158f4d714925430035c39ba054f77
Calla Chennault <51501715+callachennault@users.noreply.github.com>
2024-11-12 14:35:53 -0500

    add portal scripts directory to clone_db_wrapper

--- import-scripts/clone_db_wrapper.sh
+++ import-scripts/clone_db_wrapper.sh
@@ -4,8 +4,29 @@
 # Figures out which database is "production" versus "not production" and passes it to the clone_mysql_database script
 # Also determines which one is "blue"/"green" for the deployment
 
-. /data/portal-cron/scripts/automation-environment.sh
+for i in "$@"; do
+case $i in
+    -p=*|--portal-scripts-directory=*)
+    PORTAL_SCRIPTS_DIRECTORY="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+      # default option
+      echo "This option does not exist!  " "${i#*=}"
+    ;;
+esac
+if [ -z $PORTAL_SCRIPTS_DIRECTORY ]; then
+    PORTAL_SCRIPTS_DIRECTORY="/data/portal-cron/scripts"
+fi
 
+if [ ! -f $PORTAL_SCRIPTS_DIRECTORY/automation-environment.sh ] ; then
+  echo "`date`: Unable to locate automation_env, exiting..."
+  exit 1
+fi
+
+source $PORTAL_SCRIPTS_DIRECTORY/automation-environment.sh
+
+# Create tmp directory for processing
 tmp=$PORTAL_HOME/tmp/import-cron-genie
 if ! [ -d "$tmp" ] ; then
     if ! mkdir -p "$tmp" ; then
@@ -19,9 +40,9 @@ fi
 
 # This file will be used throughout entire import job to determine directionality
 SET_UPDATE_PROCESS_OUTPUT_FILEPATH="$tmp/update_process_output.txt"
-MANAGE_DATABASE_TOOL_PROPERTIES_FILEPATH=$PORTAL_HOME/scripts/airflowdb.properties
-DROP_TABLES_FROM_MYSQL_DATABASE_SCRIPT_FILEPATH=$PORTAL_HOME/scripts/drop_tables_in_mysql_database.sh
-CLONE_MYSQL_DATABASE_SCRIPT_FILEPATH=$PORTAL_HOME/scripts/clone_mysql_database.sh
+MANAGE_DATABASE_TOOL_PROPERTIES_FILEPATH=$PORTAL_SCRIPTS_DIRECTORY/airflowdb.properties
+DROP_TABLES_FROM_MYSQL_DATABASE_SCRIPT_FILEPATH=$PORTAL_SCRIPTS_DIRECTORY/drop_tables_in_mysql_database.sh
+CLONE_MYSQL_DATABASE_SCRIPT_FILEPATH=$PORTAL_SCRIPTS_DIRECTORY/clone_mysql_database.sh
 
 if ! $SET_UPDATE_PROCESS_SCRIPT_FILEPATH $MANAGE_DATABASE_TOOL_PROPERTIES_FILEPATH running > "$SET_UPDATE_PROCESS_OUTPUT_FILEPATH" ; then
     echo "Error during execution of $SET_UPDATE_PROCESS_SCRIPT_FILEPATH : could not set running state" >&2
@@ -29,7 +50,7 @@ if ! $SET_UPDATE_PROCESS_SCRIPT_FILEPATH $MANAGE_DATABASE_TOOL_PROPERTIES_FILEPA
     #TODO : recover from this error
 fi
 
-current_production_database_color=$(sh $PORTAL_HOME/scripts/get_database_currently_in_production.sh $MANAGE_DATABASE_TOOL_PROPERTIES_FILEPATH)
+current_production_database_color=$(sh $PORTAL_SCRIPTS_DIRECTORY/get_database_currently_in_production.sh $MANAGE_DATABASE_TOOL_PROPERTIES_FILEPATH)
 source_database_color="unset"
 destination_database_color="unset"
 if [ ${current_production_database_color:0:4} == "blue" ] ; then
