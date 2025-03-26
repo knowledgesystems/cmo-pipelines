edc96ad8a35cc66d75c84f3cc98da3190b98725c
Calla Chennault <51501715+callachennault@users.noreply.github.com>
2025-03-26 17:19:31 -0400

    use production config filepath in genie dag

--- dags/import_genie_dag.py
+++ dags/import_genie_dag.py
@@ -44,7 +44,7 @@ with DAG(
 
     conn_id = "genie_importer_ssh"
     import_scripts_path = "/data/portal-cron/scripts"
-    db_properties_filepath="/data/portal-cron/pipelines-credentials/manage_genie_database_update_tools.properties.test"
+    db_properties_filepath="/data/portal-cron/pipelines-credentials/manage_genie_database_update_tools.properties"
     
     """
     Parses and validates DAG arguments
