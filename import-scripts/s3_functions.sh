bbdc81467fc1b3004685c33b81a512c08a2230f2
Manda Wilson <1458628+mandawilson@users.noreply.github.com>
2025-04-11 09:48:20 -0400

    changed s3 upload so deleted files are removed -- but not previous versions

--- import-scripts/s3_functions.sh
+++ import-scripts/s3_functions.sh
@@ -5,7 +5,8 @@ if ! [ -n "$PORTAL_HOME" ] ; then
     exit 1
 fi
 
-# Uploads a file or directory to an s3 bucket
+# Syncs a directory to an s3 bucket
+# Files that have been removed in the local directory will be removed in the bucket
 function upload_to_s3() {
     DIR_TO_UPLOAD="$1"
     DIR_NAME_IN_S3="$2"
@@ -19,7 +20,7 @@ function upload_to_s3() {
 
     # Authenticate and upload into S3 bucket
     $PORTAL_HOME/scripts/authenticate_service_account.sh eks
-    aws s3 cp --recursive $DIR_TO_UPLOAD s3://$BUCKET_NAME/$DIR_NAME_IN_S3 --profile saml
+    aws s3 sync $DIR_TO_UPLOAD s3://$BUCKET_NAME/$DIR_NAME_IN_S3 --delete --profile saml
     if [ $? -ne 0 ] ; then
         echo "`date`: Failed to upload '$DIR_TO_UPLOAD' to S3, exiting..."
         exit 1
