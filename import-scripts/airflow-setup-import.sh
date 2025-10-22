1f1f1048265570dc7efbecfb9003b2300c9dbcf8
James Ko <koj2@mskcc.org>
2025-10-22 21:08:25 +0000

    kill tail process on script exit

--- import-scripts/airflow-setup-import.sh
+++ import-scripts/airflow-setup-import.sh
@@ -88,7 +88,10 @@ tmp="$PORTAL_HOME/tmp/$TMP_DIR_NAME"
 JAVA_IMPORTER_ARGS="$JAVA_SSL_ARGS -Dspring.profiles.active=dbcp -Djava.io.tmpdir=$tmp -ea -cp $IMPORTER_JAR_FILENAME org.mskcc.cbio.importer.Admin"
 
 # Direct importer logs to stdout
+# Make sure to kill the tail process on exit so we don't hang the script
 tail -f "$PORTAL_HOME/logs/$LOG_FILE_NAME" &
+TAIL_PID=$!
+trap 'kill "$TAIL_PID" 2>/dev/null; wait "$TAIL_PID" 2>/dev/null' EXIT INT TERM
 
 if ! is_mysql_import; then
     echo "Destination DB color: $destination_database_color"
