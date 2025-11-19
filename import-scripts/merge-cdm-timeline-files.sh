1b5a6303f461a769cd2b93ed94d451f47e74449e
Calla Chennault <51501715+callachennault@users.noreply.github.com>
2025-11-18 20:28:06 -0500

    try update to cdm-timeline-files merge

--- import-scripts/merge-cdm-timeline-files.sh
+++ import-scripts/merge-cdm-timeline-files.sh
@@ -6,17 +6,18 @@ if ! [ -n "$PORTAL_HOME" ] ; then
 fi
 
 if [ ! -f $PORTAL_HOME/scripts/automation-environment.sh ] ; then
-    echo "`date`: Unable to locate automation_env, exiting..."
+    echo "`date`: Unable to locate automation-env, exiting..."
     exit 1
 fi
 
 source $PORTAL_HOME/scripts/automation-environment.sh
 
-COHORT=$1
-OUTPUT_DIR=""
-MERGE_DIRS=()
+COHORT="$1"
+OUTPUT_DIR="$2"
+# TODO this part
+MERGE_DIRS="$3"
+MERGE_DIRS_LIST=()
 FILE_LIST=()
-COHORT_NAME_FOR_COMMIT_MSG=""
 
 function check_args() {
     if [[ -z $COHORT ]] || [[ "$COHORT" != "mixedpact" && "$COHORT" != "mskimpact" && "$COHORT" != "lymphoma_super_cohort_fmi_msk" ]]; then
@@ -32,22 +33,20 @@ function usage {
 
 function set_cohort_filepaths() {
     # Set data directory paths
-    if [ "$COHORT" == "mixedpact" ] ; then
-        COHORT="MIXEDPACT"
-        OUTPUT_DIR=$MSK_MIXEDPACT_DATA_HOME
-        MERGE_DIRS=("$MSK_IMPACT_DATA_HOME" "$MSK_HEMEPACT_DATA_HOME" "$MSK_RAINDANCE_DATA_HOME" "$MSK_ARCHER_UNFILTERED_DATA_HOME" "$MSK_ACCESS_DATA_HOME")
-    elif [ "$COHORT" == "mskimpact" ] ; then
-        COHORT="MSKSOLIDHEME"
-        OUTPUT_DIR=$MSK_SOLID_HEME_DATA_HOME
-        MERGE_DIRS=("$MSK_IMPACT_DATA_HOME" "$MSK_HEMEPACT_DATA_HOME" "$MSK_ACCESS_DATA_HOME")
-    elif [ "$COHORT" == "lymphoma_super_cohort_fmi_msk" ] ; then
-        COHORT="LYMPHOMA_SUPER_COHORT"
-        OUTPUT_DIR=$LYMPHOMA_SUPER_COHORT_DATA_HOME
-        MERGE_DIRS=("$MSK_IMPACT_DATA_HOME" "$MSK_HEMEPACT_DATA_HOME" "$FMI_BATLEVI_DATA_HOME")
-    fi
+    #if [ "$COHORT" == "mixedpact" ] ; then
+    #    OUTPUT_DIR=$MSK_MIXEDPACT_DATA_HOME
+    #    MERGE_DIRS=("$MSK_IMPACT_DATA_HOME" "$MSK_HEMEPACT_DATA_HOME" "$MSK_RAINDANCE_DATA_HOME" "$MSK_ARCHER_UNFILTERED_DATA_HOME" "$MSK_ACCESS_DATA_HOME")
+    #elif [ "$COHORT" == "mskimpact" ] ; then
+    #    OUTPUT_DIR=$MSK_SOLID_HEME_DATA_HOME
+    #    MERGE_DIRS=("$MSK_IMPACT_DATA_HOME" "$MSK_HEMEPACT_DATA_HOME" "$MSK_ACCESS_DATA_HOME")
+    #elif [ "$COHORT" == "lymphoma_super_cohort_fmi_msk" ] ; then
+    #    OUTPUT_DIR=$LYMPHOMA_SUPER_COHORT_DATA_HOME
+    #    MERGE_DIRS=("$MSK_IMPACT_DATA_HOME" "$MSK_HEMEPACT_DATA_HOME" "$FMI_BATLEVI_DATA_HOME")
+    #fi
+    IFS=' ' read -r -a MERGE_DIRS_LIST <<< "$MERGE_DIRS"
 
     # This gets the base filename for each of the timeline files
-    FILE_LIST=($(cd ${MERGE_DIRS[0]} && ls data_timeline_*.txt))
+    FILE_LIST=($(cd ${MERGE_DIRS_LIST[0]} && ls data_timeline_*.txt))
     
     # Check that required directories exist
     if [ ! -d $OUTPUT_DIR ] || [ ! -d $MSK_IMPACT_DATA_HOME ] || [ ! -d $MSK_HEMEPACT_DATA_HOME ] || [ ! -d $MSK_RAINDANCE_DATA_HOME ] || [ ! -d $MSK_ARCHER_UNFILTERED_DATA_HOME ] || [ ! -d $MSK_ACCESS_DATA_HOME ] ; then
@@ -60,7 +59,7 @@ function merge_timeline_files() {
     # Merge each type of timeline file in each data directory
     for TIMELINE_FILE in ${FILE_LIST[@]}; do
         FILES_TO_MERGE=""
-        for MERGE_DIR in ${MERGE_DIRS[@]}; do
+        for MERGE_DIR in ${MERGE_DIRS_LIST[@]}; do
             # Check if the file exists before adding to command
             FILE_TO_MERGE="$MERGE_DIR/$TIMELINE_FILE"
             if [ -f $FILE_TO_MERGE ]; then
@@ -69,7 +68,6 @@ function merge_timeline_files() {
         done
         $PYTHON3_BINARY $PORTAL_HOME/scripts/combine_files_py3.py -i $FILES_TO_MERGE -o $OUTPUT_DIR/$TIMELINE_FILE -m outer
     done
-
 }
 
 date
