d72c1138b6d50cba4b5d1c059a5d72d57fc0768c
James Ko <jamesqko@gmail.com>
2025-12-11 11:00:22 -0500

    add extra validation to make sure the instance class is valid according to AWS

--- import-scripts/rds_functions.sh
+++ import-scripts/rds_functions.sh
@@ -26,10 +26,38 @@ rds_stop() {
     aws rds wait db-instance-stopped --db-instance-identifier "$id"
 }
 
+# Validate that a class is orderable for the instance's engine/version
+rds_validate_class() {
+    local id="$1"
+    local new_class="$2"
+    local engine engine_version
+
+    read -r engine engine_version <<<"$(aws rds describe-db-instances \
+        --db-instance-identifier "$id" \
+        --query 'DBInstances[0].[Engine,EngineVersion]' \
+        --output text)"
+
+    if [[ -z "$engine" || -z "$engine_version" ]]; then
+        echo "Unable to determine engine/version for '$id'" >&2
+        return 1
+    fi
+
+    aws rds describe-orderable-db-instance-options \
+        --engine "$engine" \
+        --engine-version "$engine_version" \
+        --query "OrderableDBInstanceOptions[?DBInstanceClass=='$new_class'].DBInstanceClass" \
+        --output text | grep -qx "$new_class"
+}
+
 # Explicitly set class
 rds_set_class() {
     local id="$1"
     local new_class="$2"
+
+    if ! rds_validate_class "$id" "$new_class"; then
+        echo "Invalid DB instance class '$new_class' for '$id'" >&2
+        return 1
+    fi
 
     aws rds modify-db-instance \
         --db-instance-identifier "$id" \
