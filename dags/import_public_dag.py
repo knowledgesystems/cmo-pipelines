119b521346b20ea9e6ad0e12a2fd0ff07cfb9916
James Ko <koj2@mskcc.org>
2026-01-12 12:53:28 -0500

    Temporary - remove scale up and down RDS node tasks from public DAG

--- dags/import_public_dag.py
+++ dags/import_public_dag.py
@@ -12,10 +12,9 @@ from dags.import_base import ImporterConfig, build_import_dag
 
 
 def _wire(tasks: dict[str, object]) -> None:
-    tasks["data_repos"] >> tasks["verify_management_state"] >> [tasks["fetch_data"], tasks["scale_up_rds_node"]]
-    tasks["scale_up_rds_node"] >> tasks["clone_database"]
+    tasks["data_repos"] >> tasks["verify_management_state"] >> [tasks["fetch_data"], tasks["clone_database"]]
     [tasks["fetch_data"], tasks["clone_database"]] >> tasks["setup_import"]
-    tasks["setup_import"] >> tasks["import_sql"] >> tasks["import_clickhouse"] >> tasks["transfer_deployment"] >> tasks["scale_down_rds_node"] >> tasks["set_import_abandoned"] >> tasks["cleanup_data"]
+    tasks["setup_import"] >> tasks["import_sql"] >> tasks["import_clickhouse"] >> tasks["transfer_deployment"] >> tasks["set_import_abandoned"] >> tasks["cleanup_data"]
 
 _PUBLIC_CONFIG = ImporterConfig(
     dag_id="import_public_dag",
@@ -26,14 +25,12 @@ _PUBLIC_CONFIG = ImporterConfig(
     data_nodes=("importer_ssh", "pipelines3_ssh"),
     task_names=(
         "verify_management_state",
-        "scale_up_rds_node",
         "clone_database",
         "fetch_data",
         "setup_import",
         "import_sql",
         "import_clickhouse",
         "transfer_deployment",
-        "scale_down_rds_node",
         "set_import_abandoned",
         "cleanup_data",
     ),
