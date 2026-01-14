7e35f21e2fe3ab0a1cab26e4677a45aef48b41b2
Calla Chennault <51501715+callachennault@users.noreply.github.com>
2026-01-14 18:24:43 -0500

    fix message format

--- dags/test_slack_notifs_dag.py
+++ dags/test_slack_notifs_dag.py
@@ -1,14 +1,14 @@
 import pendulum
+from datetime import datetime
 
 from airflow.decorators import dag, task
 from airflow.providers.slack.notifications.slack_webhook import send_slack_webhook_notification
 
 dag_failure_slack_webhook_notification = send_slack_webhook_notification(
-    slack_webhook_conn_id="slack_default", text="The dag {{ dag.dag_id }} failed"
+    slack_webhook_conn_id="slack_default", text=f"DAG **{{ dag.dag_id }}** failed at {datetime.now()}"
 )
-
 dag_success_slack_webhook_notification = send_slack_webhook_notification(
-    slack_webhook_conn_id="slack_default", text="The dag {{ dag.dag_id }} succeeded"
+    slack_webhook_conn_id="slack_default", text=f"DAG **{{ dag.dag_id }}** succeeded at {datetime.now()}"
 )
 
 @dag(
