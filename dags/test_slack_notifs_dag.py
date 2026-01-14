7bf8f07ae30bc81f5db86ab34c74ced1e9dc103e
Calla Chennault <51501715+callachennault@users.noreply.github.com>
2026-01-14 18:04:17 -0500

    add dag to test slack callbacks

---
+++ dags/test_slack_notifs_dag.py
@@ -0,0 +1,35 @@
+import pendulum
+
+from airflow.decorators import dag, task
+from airflow.providers.slack.notifications.slack_webhook import send_slack_webhook_notification
+
+dag_failure_slack_webhook_notification = send_slack_webhook_notification(
+    slack_webhook_conn_id="slackwebhook", text="The dag {{ dag.dag_id }} failed"
+)
+
+dag_success_slack_webhook_notification = send_slack_webhook_notification(
+    slack_webhook_conn_id="slackwebhook", text="The dag {{ dag.dag_id }} succeeded"
+)
+
+@dag(
+    schedule=None,
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=['example'],
+    default_args={
+        'email_on_failure': False,
+        'email_on_retry': False,
+    },
+    on_failure_callback=[dag_failure_slack_webhook_notification],
+    on_success_callback=[dag_success_slack_webhook_notification]
+)
+def test_slack_notifs_dag():    
+    @task
+    def hello():
+        print('Hello world')
+
+    hello
+
+
+# Execute the dag
+test_slack_notifs_dag()
