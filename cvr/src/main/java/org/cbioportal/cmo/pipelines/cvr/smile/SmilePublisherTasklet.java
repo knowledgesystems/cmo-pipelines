5bb59533351fc5aa6e2b9b5e95ce8d513d0fb4be
ao508 <15623749+ao508@users.noreply.github.com>
2026-01-22 10:14:51 -0800

    Update SMILE message contents (#1287)
    SMILE message contents from the CVR fetcher now also include a property resolved
    by the calculation of the median exonic variant allele frequencies.
    
    cvr/pom.xml has a new dependency for apache commons math.
    
    Signed-off-by: Angelica Ochoa <ochoaa@mskcc.org>
    Co-authored-by: Angelica Ochoa <ochoaa@mskcc.org>

--- cvr/src/main/java/org/cbioportal/cmo/pipelines/cvr/smile/SmilePublisherTasklet.java
+++ cvr/src/main/java/org/cbioportal/cmo/pipelines/cvr/smile/SmilePublisherTasklet.java
@@ -1,14 +1,17 @@
 package org.cbioportal.cmo.pipelines.cvr.smile;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.*;
 import java.util.*;
+import org.apache.commons.math.stat.descriptive.rank.Median;
 import org.apache.log4j.Logger;
 import org.cbioportal.cmo.pipelines.cvr.CVRUtilities;
 import org.cbioportal.cmo.pipelines.cvr.CvrSampleListUtil;
 import org.cbioportal.cmo.pipelines.cvr.model.CVRData;
 import org.cbioportal.cmo.pipelines.cvr.model.CVRMergedResult;
 import org.cbioportal.cmo.pipelines.cvr.model.CVRMetaData;
+import org.cbioportal.cmo.pipelines.cvr.model.CVRSnp;
 import org.mskcc.cmo.messaging.Gateway;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
@@ -62,16 +65,13 @@ public class SmilePublisherTasklet implements Tasklet {
         }
         // iterate through json file and if sample id is in list of samples that were
         // consumed successfully then publish its metadata to smile
-        List<CVRMetaData> sampleMetadataList = loadSampleMetadataFromJson();
-        for (CVRMetaData sample : sampleMetadataList) {
-            log.info("Publishing sample metadata to SMILE: " + sample.getDmpSampleId());
-            messagingGateway.publish(smileDmpNewSampleTopic, mapper.writeValueAsString(sample));
-        }
+        Integer samplesPublished = loadAndPublishDmpSamplesFromJson();
+        log.info("Total samples published to SMILE: " + samplesPublished);
 
         return RepeatStatus.FINISHED;
     }
 
-    private List<CVRMetaData> loadSampleMetadataFromJson() {
+    private Integer loadAndPublishDmpSamplesFromJson() throws JsonProcessingException, Exception {
         File cvrFile = new File(jsonFilename);
         CVRData cvrData = new CVRData();
         try {
@@ -81,15 +81,46 @@ public class SmilePublisherTasklet implements Tasklet {
             throw new ItemStreamException(e);
         }
 
+        int samplesPublished = 0;
         // add sample metadata to list that should be published to smile
         Set<String> samplesToPublish = cvrSampleListUtil.getSmileSamplesToPublishList();
-        List<CVRMetaData> sampleMetadataList = new ArrayList();
         for (CVRMergedResult result : cvrData.getResults()) {
-            if (samplesToPublish.contains(result.getMetaData().getDmpSampleId())) {
-                sampleMetadataList.add(result.getMetaData());
+            String dmpId = result.getMetaData().getDmpSampleId();
+            if (samplesToPublish.contains(dmpId)) {
+                Map<String, Object> sampleMap = mapper.convertValue(result.getMetaData(), Map.class);
+                // add recommended_coverage to sample data map for smile
+                String recommendedCoverage = resolveRecommendedCoverageFromVafs(result.getAllSignedoutCvrSnps());
+                sampleMap.put("recommended_coverage", recommendedCoverage);
+
+                log.info("Publishing sample metadata to SMILE: " + dmpId);
+                messagingGateway.publish(smileDmpNewSampleTopic, mapper.writeValueAsString(sampleMap));
+                samplesPublished++;
             }
         }
-        return sampleMetadataList;
+        return samplesPublished;
     }
 
+    private String resolveRecommendedCoverageFromVafs(List<CVRSnp> variants) {
+        List<Double> vafs = new ArrayList<>();
+        for (CVRSnp v : variants) {
+            if (v.getTumorAd() == null || v.getTumorDp() == null) {
+                continue;
+            }
+            double vaf = (double) v.getTumorAd() / v.getTumorDp();
+            vafs.add(vaf);
+        }
+
+        // if no valid vafs calculated then return default 'Missing_Data'
+        if (vafs.isEmpty()) {
+            return "Missing_Data";
+        }
+
+        Median med = new Median();
+        double[] values = new double[vafs.size()];
+        for (int i=0;i<vafs.size();i++) {
+            values[i] = vafs.get(i);
+        }
+        Double medianExonicVaf = med.evaluate(values);
+        return medianExonicVaf >= 0.15 ? "Standard_Coverage" : "Higher_Coverage";
+    }
 }
