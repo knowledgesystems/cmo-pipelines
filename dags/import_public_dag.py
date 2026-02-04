c722bd277a516a77910df4ae8913ed086edc0167
James Ko <jamesqko@gmail.com>
2026-02-04 16:35:38 -0500

    roll out slack changes to genie/public dags

--- dags/import_public_dag.py
+++ dags/import_public_dag.py
@@ -14,7 +14,7 @@ from dags.import_base import ImporterConfig, build_import_dag
 def _wire(tasks: dict[str, object]) -> None:
     tasks["data_repos"] >> tasks["verify_management_state"] >> [tasks["fetch_data"], tasks["clone_database"]]
     [tasks["fetch_data"], tasks["clone_database"]] >> tasks["setup_import"]
-    tasks["setup_import"] >> tasks["import_sql"] >> tasks["import_clickhouse"] >> tasks["transfer_deployment"] >> tasks["cleanup_data"]
+    tasks["setup_import"] >> tasks["import_sql"] >> tasks["import_clickhouse"] >> tasks["transfer_deployment"] >> tasks["send_update_notification"] >> tasks["cleanup_data"]
 
 _PUBLIC_CONFIG = ImporterConfig(
     dag_id="import_public_dag",
@@ -31,6 +31,7 @@ _PUBLIC_CONFIG = ImporterConfig(
         "import_sql",
         "import_clickhouse",
         "transfer_deployment",
+        "send_update_notification",
         "cleanup_data",
         "set_import_abandoned",
     ),
