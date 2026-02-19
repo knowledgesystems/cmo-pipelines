3be77e1ec1ee54aae64ae0715c91ee418765cd87
James Ko <jamesqko@gmail.com>
2026-02-19 14:36:40 -0500

    no-op change

--- import-scripts/dmp-import-vars-functions.sh
+++ import-scripts/dmp-import-vars-functions.sh
@@ -95,6 +95,7 @@ function addDataTypeCaseLists {
     GENERATE_CASE_LISTS_SCRIPT="$PORTAL_HOME/scripts/generate_case_lists.py"
     CASE_LIST_CONFIG_FILEPATH="$PORTAL_HOME/scripts/case_list_config.tsv"
     CASE_LISTS_DIRECTORY="$STUDY_DATA_DIRECTORY/case_lists"
+    
 
     $PYTHON_BINARY $GENERATE_CASE_LISTS_SCRIPT \
         --case-list-config-file $CASE_LIST_CONFIG_FILEPATH \
