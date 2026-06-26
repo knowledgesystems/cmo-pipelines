46556da71a48f4452c4928f6a72049e28a7eac50
Calla Chennault <51501715+callachennault@users.noreply.github.com>
2026-06-26 13:43:20 -0400

    Fix Docker image build and saml2aws authentication
    - Replace ClickHouse self-installer with apt install clickhouse-client
      to avoid disk exhaustion during image build
    - Replace docker-run saml2aws with direct saml2aws binary invocation
    - Add IRSA bypass in authenticate_service_account.sh: skip saml2aws
      when AWS_WEB_IDENTITY_TOKEN_FILE is set (EKS pod with IRSA)
    - Add new import-scripts and test results

---
+++ import-scripts/access_download.sh
@@ -0,0 +1,77 @@
+COHORT="mskaccess"
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_clinical_patient.txt --version-id Zno4mv1zOIUHTjvWL1rG8Nk4KlJFuG3s --profile saml data_clinical_patient.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_clinical_sample.txt --version-id f9IgwY2JtQRjFkUXAE0WbTal.u4yUGmq --profile saml data_clinical_sample.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_cancer_presence.txt --version-id _I2t0aNEpx6xXNl2rRVlJc_._uTuB0dt --profile saml data_timeline_cancer_presence.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_diagnosis.txt --version-id U8kkqNmWXFEVsZeg5zBpwHi8wKhOp9OY --profile saml data_timeline_diagnosis.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_ecog_kps.txt --version-id Ah4EY2NHSMqH0gqt.tHysBv6JCtt7fs. --profile saml data_timeline_ecog_kps.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_follow_up.txt --version-id z6qwYvAUCrc6fr05lKgQ5wOt2_scg07R --profile saml data_timeline_follow_up.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_gleason.txt --version-id juDV5csnMQ4FpIDzX9Lxkmf3bHdQ9CXm --profile saml data_timeline_gleason.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_mmr.txt --version-id glTPJ7NHcb069PWVAiuMJKYuE2FfaIgq --profile saml data_timeline_mmr.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_pdl1.txt --version-id d47S0kdTbrReiprHiuqDpDU_Mx6gsVdi --profile saml data_timeline_pdl1.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_prior_meds.txt --version-id W3oy9YAt96QTPbFHCYJcfJtsY5EsHLac --profile saml data_timeline_prior_meds.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_progression.txt --version-id mz4ybqxgy.PInF9xGhwERzaoMcTVpaDR --profile saml data_timeline_progression.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_radiation.txt --version-id NhLlhhtKk0tceQNzKpiSasGlG7CfBKMB --profile saml data_timeline_radiation.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_specimen_surgery.txt --version-id mfKmft59DQPv6lf5uDpfjC4sdvr1HscD --profile saml data_timeline_specimen_surgery.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_specimen.txt --version-id PhTFRrz2hZW26VD6tDZyMP9bmo8yZUbJ --profile saml data_timeline_specimen.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_surgery.txt --version-id QsQer7WOpAjNQwmL.qMXflHIWQj68KU_ --profile saml data_timeline_surgery.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_treatment.txt --version-id 5laWd9KTkadunjdSNYoSf7M1H9NS88na --profile saml data_timeline_treatment.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_tumor_sites.txt --version-id j9xQNSBGKZOclmyAsjVYrZ6_QKC_u22U --profile saml data_timeline_tumor_sites.txt
+
+
+COHORT="mskimpact"
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_clinical_patient.txt --version-id ZqJaakrEXdiPITq2iV7jIL9wy8kZ0AsL --profile saml data_clinical_patient.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_clinical_sample.txt --version-id B1MiQ9qy0EJ_eYvgTcY8MaO3HJbcJUzk --profile saml data_clinical_sample.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_cancer_presence.txt --version-id kA6.O1M4.gdiRk.FmKJZMcKx44fzZdUd --profile saml data_timeline_cancer_presence.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_diagnosis.txt --version-id ..Hqep7B5VMu6XfOufhPUVf7RDTPU7ws --profile saml data_timeline_diagnosis.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_ecog_kps.txt --version-id UXcYraKpwm_M.8tseDcLyB8gEysGQzFc --profile saml data_timeline_ecog_kps.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_follow_up.txt --version-id KqY63DX6OalV7qXKQxsiGQNTMNgIjV4C --profile saml data_timeline_follow_up.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_gleason.txt --version-id IrfePzmNECN1pgdn04ox8UlUJnc3zVmA --profile saml data_timeline_gleason.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_mmr.txt --version-id .bkaUfOYxzKKElFuJPGJx9V4plVlVu7h --profile saml data_timeline_mmr.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_pdl1.txt --version-id TguzdJB95R11FCwkXKBbkaSuRMsOcDKj --profile saml data_timeline_pdl1.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_prior_meds.txt --version-id UXXhBxHrwghbiW27Qi9TUQYa8jkEfHW. --profile saml data_timeline_prior_meds.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_progression.txt --version-id dbjazrFSE6et1BbUXGQ8GAAWnPQ0nKDb --profile saml data_timeline_progression.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_radiation.txt --version-id bfETjmPcvZAz9St0E3fAu8oeBjTTZnwD --profile saml data_timeline_radiation.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_specimen_surgery.txt --version-id pFC.aWN8jNMpIcZuolMma7ZLf4xwpCJW --profile saml data_timeline_specimen_surgery.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_specimen.txt --version-id SnwR5MnR7lz.KitdnMeV.47K4bUsD44z --profile saml data_timeline_specimen.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_surgery.txt --version-id 0o.rYNt5S04JaSlEtJvK0OTpjjb8_Y3v --profile saml data_timeline_surgery.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_treatment.txt --version-id 3RXPfhvGAG6y2rgsU1afaASVhcjwLeap --profile saml data_timeline_treatment.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_tumor_sites.txt --version-id 8G6v66FzXxbUzuMDg_bs0xfTJP2RcoII --profile saml data_timeline_tumor_sites.txt
+
+
+COHORT="mskimpact_heme"
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_clinical_patient.txt --version-id U1MlTYhn7YtJkLQkgvv4ENfdGao1.flm --profile saml data_clinical_patient.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_clinical_sample.txt --version-id oVbKhK3AxiOuxYbaIsg9Kfrdho4iz2YV --profile saml data_clinical_sample.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_cancer_presence.txt --version-id dX1c_4gQMisY.JpoItnkH9OtLwezMdUb --profile saml data_timeline_cancer_presence.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_diagnosis.txt --version-id erEuqwHLlipaz_xKMjT947in7x45.JGQ --profile saml data_timeline_diagnosis.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_ecog_kps.txt --version-id ZisX2ZK7VbGO0oX8QZswZ.COxO23rlS5 --profile saml data_timeline_ecog_kps.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_follow_up.txt --version-id iWJFPzgv03Xmue1fK_jUhsHbIdCAcygp --profile saml data_timeline_follow_up.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_gleason.txt --version-id QfnURNm_J.WxzeEr86RvCfvDM_I5UyXh --profile saml data_timeline_gleason.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_mmr.txt --version-id 0R.cjvm3TY0PKHjhb_UUSGflsdPk9RXg --profile saml data_timeline_mmr.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_pdl1.txt --version-id jLmNrWt.9g6uxf0imA9VEZXxfVp9mdYW --profile saml data_timeline_pdl1.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_prior_meds.txt --version-id mqs1wuLnQNNbU.mLipPgxpeni16mEY0R --profile saml data_timeline_prior_meds.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_progression.txt --version-id XhqevR3UDHXSVA1HaW9uX.iYF9ebBxMh --profile saml data_timeline_progression.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_radiation.txt --version-id OXAOk5akYQxd..yNBck452XZOFb8U7sD --profile saml data_timeline_radiation.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_specimen_surgery.txt --version-id M_Xw9B4zVkEb141YrBOtNR5Zn2BHfxTp --profile saml data_timeline_specimen_surgery.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_specimen.txt --version-id 8mM3X1OmIJJzexUpH6quprk3oU3sThtF --profile saml data_timeline_specimen.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_surgery.txt --version-id XeY8MsWwMOvi4CRSlM0AHLvdB1pdxXG. --profile saml data_timeline_surgery.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_treatment.txt --version-id FbO1_7fbARiM4LGKDWp630oOORCerUvq --profile saml data_timeline_treatment.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_tumor_sites.txt --version-id j9zPzUJmNQ6k8T8AI8e.RVu1._pBlnqy --profile saml data_timeline_tumor_sites.txt
+
+COHORT="mskarcher"
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_clinical_patient.txt --version-id VZmrIbG2wQlhiWOPCQ9pqy689Cf.C0UU --profile saml data_clinical_patient.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_clinical_sample.txt --version-id We3LfTdNgzTSVYPJhW71JjTwGaxMfCoh --profile saml data_clinical_sample.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_cancer_presence.txt --version-id E3CcBgJiG7KFtbrpVV_NWXPhGeVheUZo --profile saml data_timeline_cancer_presence.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_diagnosis.txt --version-id 3UgaMM0BAz3zc8dfT04drHoSsjrTahFP --profile saml data_timeline_diagnosis.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_ecog_kps.txt --version-id gyzeRjEiUayyY7UnPd30lOj5L5A.s.ah --profile saml data_timeline_ecog_kps.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_follow_up.txt --version-id HWjS.s_LZDZ0w8zyx9hnKg2bH6VcH9N. --profile saml data_timeline_follow_up.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_gleason.txt --version-id gfWh0ZE7M.spV_Y4YnlBAp1_1HWqduZR --profile saml data_timeline_gleason.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_mmr.txt --version-id FkjiKXseCNA4lMMAb510N1SHTaZ8BWrh --profile saml data_timeline_mmr.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_pdl1.txt --version-id Tx9ehDeTcDE4WCpffnMsQjmJxsuz1uYH --profile saml data_timeline_pdl1.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_prior_meds.txt --version-id ZWyYjdSYrUkmqy8vsqdaUlAssAct7FLg --profile saml data_timeline_prior_meds.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_progression.txt --version-id BCzY6AunIfp0iRE750R4tZCGYGAMDtSd --profile saml data_timeline_progression.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_radiation.txt --version-id ugMdYkbzrZBNkkvvMnsg8RMuodpph0UN --profile saml data_timeline_radiation.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_specimen_surgery.txt --version-id _i.iECaOA1HyBhCzssV_SeGLPp9RnGxe --profile saml data_timeline_specimen_surgery.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_specimen.txt --version-id 4e.5Uw.cdsHIc3zrE0MAyXB9dXwmdo5J --profile saml data_timeline_specimen.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_surgery.txt --version-id QW.3KbKfx9awJj.2zIURIeSukr64._Uj --profile saml data_timeline_surgery.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_treatment.txt --version-id VSnzKVpDMhc1YhxMUAts5dK9zm5yXTq_ --profile saml data_timeline_treatment.txt
+aws s3api get-object --bucket cdm-deliverable --key msk-chord/$COHORT/data_timeline_tumor_sites.txt --version-id gnypQ_N4og5vEeM2C584p_0bBhG0MTde --profile saml data_timeline_tumor_sites.txt
